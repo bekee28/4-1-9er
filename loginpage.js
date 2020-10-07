150ddec0e6f68e2d257bfad56ba1fee8014a1a32
Emmanuel Aluo <72333988+bekee28@users.noreply.github.com>
2020-10-07 14:58:20 -0700

    Add files via upload

---
+++ loginpage.js
@@ -0,0 +1,6 @@
+var login = document.querySelector("#login")
+var form = document.querySelector("form")
+
+login.addEventListener("click", function(){
+    form.setAttribute("action", "https://music.youtube.com/watch?v=r7jibbi1uL4&list=RDAMVMr7jibbi1uL4")
+})
