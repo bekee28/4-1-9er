851bd508b06b6d9b6ffdf3a03e845c5d85c1794f
Emmanuel Aluo <72333988+bekee28@users.noreply.github.com>
2021-02-03 12:29:00 -0800

    Delete loginpage.js

--- loginpage.js
+++
@@ -1,6 +0,0 @@
-var login = document.querySelector("#login")
-var form = document.querySelector("form")
-
-login.addEventListener("click", function(){
-    form.setAttribute("action", "https://music.youtube.com/watch?v=r7jibbi1uL4&list=RDAMVMr7jibbi1uL4")
-})
